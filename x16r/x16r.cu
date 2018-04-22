41b09abe565d1631dc6dd92d5b54600945fc942b
Graeme Seaton <graemes@graemes.com>
2018-04-22 14:08:18 +0100

    Tweak benchmark hashes

--- x16r/x16r.cu
+++ x16r/x16r.cu
@@ -657,8 +657,8 @@ static void setBenchHash() {
 	if (opt_bench_hash[0]) {
 		bool bench_hash_found = false;
 		applog(LOG_INFO, "Looking for benchmark hashing algorithm %s", opt_bench_hash);
-		uint8_t bench_algo = 0;
-		for (uint8_t j = 0; j < HASH_FUNC_COUNT; j++) {
+		//uint8_t bench_algo = 0;
+		for (uint8_t j = 0; j < (HASH_FUNC_COUNT-1); j++) {
 			// full hash?
 			if ((strcmp(algo_strings[j], opt_bench_hash) == 0)) {
 				bench_hash = algo_hashes[j];
@@ -672,7 +672,7 @@ static void setBenchHash() {
 		}
 
 		if (!bench_hash_found){
-			bench_hash = 0x67452301EFCDAB89;
+			//bench_hash = 0x67452301EFCDAB89;
 			applog(LOG_WARNING, "Specified benchmark hashing algorithm %s not found. Using default: %s", opt_bench_hash, bench_hash);
 		}
 	}
