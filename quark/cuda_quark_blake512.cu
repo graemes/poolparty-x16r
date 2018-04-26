a4d2c98a7d99b8d08158e3069f303e2fc80bd164
graemes <graemes@github>
2018-04-26 11:25:29 +0100

    cuda_check_hash removal

--- quark/cuda_quark_blake512.cu
+++ quark/cuda_quark_blake512.cu
@@ -383,6 +383,7 @@ uint32_t quark_blake512_cpu_hash_64_check(const int thr_id, const uint32_t threa
 	gpulog(LOG_INFO, thr_id, "Memory set");
 
 	quark_blake512_gpu_hash_64_check<<<grid, block>>>(threads, (uint2*)d_outputHash, startNounce, ptarget, d_resNonces[thr_id]);
+	cudaThreadSynchronize();
 //	quark_blake512_gpu_hash_64_check<<<grid, block>>>(threads, (uint2*)d_outputHash, startNounce);
 //	quark_blake512_gpu_hash_64_check<<<grid, block>>>(threads, (uint2*)d_outputHash);
 	cudaMemcpy(h_resNonces[thr_id], d_resNonces[thr_id], sizeof(uint32_t), cudaMemcpyDeviceToHost);
